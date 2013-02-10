82e3867e718993a7a3bf254bd573eeda632966a2
Will <brickhouseproject>
2013-02-10 15:30:13 -0600

    Added basic google maps example

---
+++ BasicGoogleMap/src/com/bigbrickapps/googlemap/MainActivity.java
@@ -0,0 +1,23 @@
+package com.bigbrickapps.googlemap;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+      
+    }
+    
+    
+}
